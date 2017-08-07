6869b3f1c9c0efd0831caa404d425e9c94c2dcb5
Wei Lun <weilun_95@hotmail.com>
2017-08-07 23:30:53 +0800

    implemented AddBirthdayActivity

--- app/src/main/java/com/example/weilun/birthdayreminder/AddBirthdayActivity.java
+++ app/src/main/java/com/example/weilun/birthdayreminder/AddBirthdayActivity.java
@@ -1,5 +1,6 @@
 package com.example.weilun.birthdayreminder;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,8 +8,20 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddBirthdayActivity extends AppCompatActivity {
+    private ImageView image;
+    private EditText etName, etEmail, etPhone, etDob;
+    private Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +30,36 @@ public class AddBirthdayActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        image = (ImageView) findViewById(R.id.icon);
+        etName = (EditText) findViewById(R.id.add_birthday_name);
+        etEmail = (EditText) findViewById(R.id.add_birthday_email);
+        etPhone = (EditText) findViewById(R.id.add_birthday_phone);
+        etDob = (EditText) findViewById(R.id.birthday_date);
+        aSwitch = (Switch) findViewById(R.id.show_noti);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                try {
+                    //int imageResourceId = (int)image.getTag(); //get image resource id
+                    int imageResourceId = R.drawable.ic_account_circle_black_24dp;
+                    String name = etName.getText().toString();
+                    String email = etEmail.getText().toString();
+                    String phone = etPhone.getText().toString();
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.ENGLISH);
+                    Date date = dateFormat.parse(etDob.getText().toString());
+                    Boolean isChecked = aSwitch.isChecked();
+
+                    PersonDBQueries dbQueries = new PersonDBQueries(new PersonDBHelper(getApplicationContext()));
+                    Person person = new Person(name, email, phone, date, isChecked, imageResourceId);
+                    if (dbQueries.insert(person) != 0) {
+                        Toast.makeText(AddBirthdayActivity.this, "inserted", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                } catch (ParseException e) {
+                    Toast.makeText(AddBirthdayActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/weilun/birthdayreminder/UpComingActivityFragment.java
+++ app/src/main/java/com/example/weilun/birthdayreminder/UpComingActivityFragment.java
@@ -1,10 +1,12 @@
 package com.example.weilun.birthdayreminder;
 
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,26 @@ public class UpComingActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_up_coming, container, false);
+      return  inflater.inflate(R.layout.fragment_up_coming, container, false);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        PersonDBQueries dbQuery = new PersonDBQueries(new PersonDBHelper(getActivity()));
+        String[] columns = {
+                PersonContract.PersonEntry._ID,
+                PersonContract.PersonEntry.COLUMN_NAME_NAME,
+                PersonContract.PersonEntry.COLUMN_NAME_DOB,
+                PersonContract.PersonEntry.COLUMN_NAME_IMAGERESOUCEID,
+
+        };
+        Cursor cursor = dbQuery.query(columns, null, null, null, null
+                , PersonContract.PersonEntry.COLUMN_NAME_DOB + " ASC");
+        PersonCursorAdapter adapter = new PersonCursorAdapter(getActivity(), cursor, 0);
+        ListView listView =(ListView) getActivity().findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+        listView.setEmptyView(getActivity().findViewById(R.id.no_birthday));
     }
 }
