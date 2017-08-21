56b06669e431b030c9d37a47caa630accba0cfd1
Wei Lun <weilun_95@hotmail.com>
2017-08-21 18:11:04 +0800

    added up navigation for ViewBirthdayActivity

--- app/src/main/java/com/example/weilun/birthdayreminder/ViewBirthdayActivity.java
+++ app/src/main/java/com/example/weilun/birthdayreminder/ViewBirthdayActivity.java
@@ -45,6 +45,7 @@ public class ViewBirthdayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_birthday);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
