955f13dd952149ae177c62f1ee3c263fe059650f
Wei Lun <weilun_95@hotmail.com>
2017-08-27 12:56:02 +0800

    added error handling for EditBirthdayActivity

--- app/src/main/java/com/example/weilun/birthdayreminder/EditBirthdayActivity.java
+++ app/src/main/java/com/example/weilun/birthdayreminder/EditBirthdayActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -21,6 +22,7 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class EditBirthdayActivity extends AppCompatActivity {
+    private static final String LOG_TAG = EditBirthdayActivity.class.getSimpleName();
     private Person person;
     private ImageView image;
     private EditText etName, etEmail, etPhone, etDob;
@@ -43,22 +45,32 @@ public class EditBirthdayActivity extends AppCompatActivity {
             public void onClick(View view) {
                 try {
 
-                    //TODO: set image resources id
-                    person.setImageResourceId(R.drawable.ic_account_circle_black_24dp);
-                    person.setName(etName.getText().toString());
-                    person.setEmail(etEmail.getText().toString());
-                    person.setPhone(etPhone.getText().toString());
-                    person.setDob(new SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.ENGLISH).parse(etDob.getText().toString()));
-                    person.setNotify(aSwitch.isChecked());
+                    String name = etName.getText().toString();
+                    String email = etEmail.getText().toString();
+                    String phone = etPhone.getText().toString();
 
-                    PersonDBQueries dbQueries = new PersonDBQueries(new PersonDBHelper(getApplicationContext()));
-                    if (dbQueries.update(person) != 0) {
-                        Toast.makeText(EditBirthdayActivity.this, "Updated", Toast.LENGTH_SHORT).show();
-                        finish();
-                    } else
-                        Toast.makeText(EditBirthdayActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    if(TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(phone)){
+                        Toast.makeText(EditBirthdayActivity.this, R.string.warning_message_no_fillup, Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        //TODO: set image resources id
+                        person.setImageResourceId(R.drawable.ic_account_circle_black_24dp);
+                        person.setName(name);
+                        person.setEmail(email);
+                        person.setPhone(phone);
+                        person.setDob(new SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.ENGLISH).parse(etDob.getText().toString()));
+                        person.setNotify(aSwitch.isChecked());
+
+                        PersonDBQueries dbQueries = new PersonDBQueries(new PersonDBHelper(getApplicationContext()));
+                        if (dbQueries.update(person) != 0) {
+                            Toast.makeText(EditBirthdayActivity.this, R.string.updated, Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else
+                            Toast.makeText(EditBirthdayActivity.this, R.string.db_error, Toast.LENGTH_SHORT).show();
+                    }
                 } catch (ParseException e) {
-                    Log.v("EditBIrthdayActivity", e.getMessage());
+                    Log.v(LOG_TAG, e.getMessage());
+                    Toast.makeText(EditBirthdayActivity.this, R.string.warning_message_no_fullup_date, Toast.LENGTH_SHORT).show();
                 }
             }
         });
